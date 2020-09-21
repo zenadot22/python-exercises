9f97a81e5fe9ad28f632f36898cf958a300786f8
Your Name <dotwanazenande@gmail.com>
2020-09-21 16:29:49 +0200

    calculating circumference

---
+++ day1.exercise.py
@@ -0,0 +1,12 @@
+# task1
+H = "hello "
+H_ = " how are you?"
+St_name = input( "Enter your name\n")
+print(H + St_name + H_)
+
+#task2 calculating circumference using radius
+
+rad_:float =float(input("enter radius"))
+pi: float = 3.141
+circumference = (rad_ * pi) * 2
+print(circumference)
--- day1.py
+++ day1.py
@@ -5,4 +5,9 @@ print("I was coded in " + class_name)
 class_students =  "20"
 print("There are " + class_students + " students in " + class_name)
 
+student_name = input("what is your name: ")
+print("So your name is " + student_name)
 
+student_age =int(input("How old are you:"))
+print("so in 10 years you will be...")
+print(student_age + 10)
