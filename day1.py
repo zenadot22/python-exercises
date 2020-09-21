2015b2707869fee2b711529cea186ee0ec65716b
Your Name <dotwanazenande@gmail.com>
2020-09-21 11:02:26 +0200

    First day's work

---
+++ day1.py
@@ -0,0 +1,8 @@
+print("Hello World")
+class_name = "Class 2"
+print("I was coded in " + class_name)
+
+class_students =  "20"
+print("There are " + class_students + " students in " + class_name)
+
+
