4906aa2e6de73517876743266a051d010a81bc54
Your Name <dotwanazenande@gmail.com>
2020-09-22 15:32:14 +0200

    displaying first and last letter

---
+++ day2_exercise.py
@@ -0,0 +1,6 @@
+#Accept an input from the user and display first and last given letter
+
+name = input("enter your name \n")
+First_letter  = name[0]
+Last_letter  =  name[4]
+print ("Hi i am " + name,"\n", "First letter is " +First_letter,"\n","Last letter is " + Last_letter)
